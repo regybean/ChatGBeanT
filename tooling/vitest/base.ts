b8941b0f5db2a7eaad1b8570daae9d8f8a510f89
Regybean <reggybean@gmail.com>
2026-01-17 15:57:49 +0000

    first commit

---
+++ tooling/vitest/base.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    environment: 'node',
+    globals: true,
+    env: {
+      SKIP_ENV_VALIDATION: 'true',
+    },
+    mockReset: true,
+    testTimeout: 30000,
+    hookTimeout: 30000,
+    reporters: ['verbose'],
+    passWithNoTests: true,
+    coverage: {
+      provider: 'istanbul',
+      reporter: ['text', 'json', 'html'],
+    },
+    include: [
+      'src/**/*.test.{ts,tsx}',
+      'src/**/__tests__/**/*.test.{ts,tsx}',
+      'src/**/tests/**/*.test.{ts,tsx}',
+      'src/tests/**/*.test.{ts,tsx}',
+    ],
+  },
+});
